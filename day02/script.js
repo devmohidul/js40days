1f906c5cc2c7a9d8c9c8bb2a6b5bd2ce70824986
devmohidul <mohidulhasan145@gmail.com>
2026-01-28 16:05:18 +0600

    Day 02 Task

---
+++ day02/script.js
@@ -0,0 +1,18 @@
+console.log("Day 02");
+
+var name = "Mohidul Hasan";
+console.log(name);
+
+let address = "Dhaka";
+console.log(address);
+
+const country = "Bangladesh";
+console.log(country);
+
+let student = {
+  name: "Mohidul Hasan",
+  age: "28",
+  isEnrolled: true,
+};
+
+console.log(student.name);
